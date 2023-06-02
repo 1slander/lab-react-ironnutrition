4f03309e12796cfe13cb66b2197470a8f1883102
1slander <hectorgomendez@gmail.com>
2023-06-02 15:35:02 +0100

    iteration 4

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import { useState } from 'react';
 import './App.css';
 import foods from './foods.json'
 import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+
 function App() {
 
   const [foodsList, setFoodList]= useState(foods)
+  
+
+  // const addNewFood = (food)=>{
+  //   const updatedFoodList = [...foodsList,food];
+  //   setFoodList(updatedFoodList)
+  // }
 
   
   return (
     <div className="App">
     <h2>Food List</h2>
+    <AddFoodForm foodList={foodsList} setFoodList={setFoodList}/>
     {foodsList.map((food)=>{
       return (
        <FoodBox food={{
