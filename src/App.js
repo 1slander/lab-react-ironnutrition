23cb2c37691857c7d9153ace0c633beb49a9527d
1slander <hectorgomendez@gmail.com>
2023-06-02 12:04:19 +0100

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import './App.css';
 import foods from './foods.json'
-
+import FoodBox from './components/FoodBox';
 function App() {
 
   const [foodsList, setFoodList]= useState(foods)
@@ -13,10 +13,12 @@ function App() {
     <h2>Food List</h2>
     {foodsList.map((food)=>{
       return (
-        <div>
-        <p>{food.name}</p>
-        <img src={food.image} alt={food.name} />
-      </div>
+       <FoodBox food={{
+        name : food.name,
+        calories : food.calories,
+        image: food.image,
+        servings: food.servings
+       }}/>
       )
     })}
       
