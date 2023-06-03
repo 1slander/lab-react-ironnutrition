a0dd54db21255b20896f4fcba546269e0672d909
1slander <hectorgomendez@gmail.com>
2023-06-03 09:07:15 +0100

    fixed error

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -2,52 +2,47 @@ import { Divider, Input } from 'antd';
 import { useState } from 'react';
 
 // Iteration 4
-function AddFoodForm({foodList,setFoodList}) {
+function AddFoodForm({ foodList, setFoodList }) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
 
- const [name,setName]=useState("")
- const [image,setImage]=useState("")
- const [calories,setCalories]=useState("")
- const [servings,setServings]=useState("")
+  const handleNameInput = (e) => setName(e.target.value);
+  const handleImageInput = (e) => setImage(e.target.value);
+  const handleCaloriesInput = (e) => setCalories(e.target.value);
+  const handleServingsInput = (e) => setServings(e.target.value);
 
- const handleNameInput = e => setName(e.target.value);
- const handleImageInput = e => setImage(e.target.value);
- const handleCaloriesInput = e => setCalories(e.target.value);
- const handleServingsInput = e => setServings(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newFood = { name, image, calories, servings };
+    setFoodList([newFood, ...foodList]);
 
- const handleSubmit = (e)=> {
-   e.preventDefault();
-  const newFood = { name,image,calories,servings};
-  setFoodList([newFood, ...foodList]);
-
-  setName("");
-  setImage("");
-  setServings("");
-  setCalories("");
- }
+    setName('');
+    setImage('');
+    setServings('');
+    setCalories('');
+  };
   return (
-   <div className="addFood">
+    <div className="addFood">
+      <form onSubmit={handleSubmit}>
+        <Divider>Add Food Entry</Divider>
 
-  <form onSubmit={handleSubmit}>
-      <Divider>Add Food Entry</Divider>
+        <label>Name</label>
+        <Input value={name} type="text" onChange={handleNameInput} />
 
-      <label>Name</label>
-      <Input value={name} type="text" onChange={handleNameInput} />
+        <label>Image</label>
+        <Input value={image} type="text" onChange={handleImageInput} />
 
-      <label>Image</label>
-      <Input value={image} type="text" onChange={handleImageInput} />
+        <label>Calories</label>
+        <Input value={calories} type="number" onChange={handleCaloriesInput} />
 
-      <label>Calories</label>
-      <Input value={calories} type="number" onChange={handleCaloriesInput} />
+        <label>Servings</label>
+        <Input value={servings} type="number" onChange={handleServingsInput} />
 
-      <label>Servings</label>
-      <Input value={servings} type="number" onChange={handleServingsInput} />
-
-      <button type="submit" >Create</button>
-    </form>
-  
-
-    
-   </div>
+        <button type="submit">Create</button>
+      </form>
+    </div>
   );
 }
 
